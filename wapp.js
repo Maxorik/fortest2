072e4b840e9cdf7b877fbe16e5e1aa772d5e64fd
Maxorik <gms441@mail.com>
2020-01-07 16:13:00 +0700

    wapp

---
+++ wapp.js
@@ -0,0 +1,11 @@
+﻿let wapp = document.getElementById('wapp');
+let phone = wapp.getAttribute('number');
+let path = 'https://wa.me/' + phone;
+wapp.innerHTML = '<a href="'+path+'" class="pulse-button" target="_blank"></a>';
+
+/*
+let styles = document.createElement("link");
+styles.rel="stylesheet";
+styles.href = myurl+"/includes/wapp.css";
+document.getElementsByTagName("head")[0].appendChild(styles);
+*/
