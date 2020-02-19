c8e5ae4a1f7bce180e0af82586ae713ecf81ee1b
Maxorik <gms441@mail.com>
2020-02-19 22:26:02 +0700

    wapp

--- wapp.js
+++ wapp.js
@@ -1,7 +1,13 @@
 ﻿let wapp2 = document.getElementById('wapp2');
+
+wapp2.innerHTML = "<div class='cross'>&times;</div><div class='wapp_logo'></div><div class='wapp_txt'>Нажми и получи работу <b>прямо сейчас!</b></div>";
+
 let phone2 = wapp2.getAttribute('number');
+let txt = document.querySelector('.wapp_txt');
+let logo = document.querySelector('.wapp_logo');
 let message = 'Добрый день!%20Прошу%20проконсультировать%20меня%20касательно%20открытых%20вакансий';
 let path2 = 'https://wa.me/' + phone2 + '/?text=' + message;
+
 wapp2.addEventListener('click', function call_wapp(event){
     let t = event.target;
     if(!t.classList.contains('cross')){
@@ -14,53 +20,47 @@ let cross = document.querySelector('.cross');
 cross.addEventListener('click', function hide_wapp(){
     wapp2.style.display = 'none';
 })
-//wapp2.innerHTML += '<a href="'+path2+'" class="pulse-button" target="_blank"></a>';
-//wapp2.style.display = 'flex';
-//wapp2.style.just = 'flex';
-//let pulsebutton = document.querySelector('.pulse-button');
-//wapp.style.margin = '0 auto';
-//wapp.style.position = 'absolute';
-//wapp.style.bottom = '1.5em';
-//wapp.style.left = '2em';
-//pulsebutton.style.position='fixed';
-//pulsebutton.style.bottom='20px';
-//pulsebutton.style.marginLeft='-20px';
-//pulsebutton.style.marginTop='-20px';
-//pulsebutton.style.display= 'block';
-//pulsebutton.style.width='3em';
-//pulsebutton.style.height= '3em';
-//pulsebutton.style.fontSize='1.3em';
-//pulsebutton.style.color='#fff';
-//pulsebutton.style.textShadow='0 1px 0 #1f4c76';
-//pulsebutton.style.border='1px solid #fff';
-//pulsebutton.style.boxShadow='0 0 0 0 rgba(90, 153, 220, 0.7), inset 0 1px 0 #abcbe9';
-//pulsebutton.style.borderRadius='100%';
-//pulsebutton.style.background='#fff';
-//pulsebutton.style.cursor='pointer';
-//pulsebutton.style.backgroundRepeat= 'no-repeat';
-//pulsebutton.style.backgroundSize='3em 3em';
-//pulsebutton.style.backgroundImage = 'url(https://s8.hostingkartinok.com/uploads/images/2020/01/b3d895dbcf28042c2694ad49872363ee.png)';
-//pulsebutton.style.webkitAnimation = 'pulse 1s infinite cubic-bezier(0.6, 0, 0, 1)';
-//
-//let cssAnimation = document.createElement('style');
-//cssAnimation.type = 'text/css';
-//let rules = document.createTextNode('@-webkit-keyframes pulse {'+
-//'to{box-shadow:0 0 0 15px rgba(90, 153, 220, 0), inset 0 1px 0 #abcbe9}'+
-//'}');
-//cssAnimation.appendChild(rules);
-//document.getElementsByTagName("head")[0].appendChild(cssAnimation);
+
+
+wapp2.style.position = 'absolute';
+wapp2.style.bottom = '0';
+wapp2.style.left = '0';
+wapp2.style.width = '20%';
+wapp2.style.height = '10%';
+wapp2.style.backgroundColor = '#15a259';
+wapp2.style.borderTopRightRadius = '20px';
+wapp2.style.display = 'flex';
+wapp2.style.justifyContent = 'space-around';
+wapp2.style.alignItems = 'center';
+wapp2.style.cursor = 'pointer';
+
+logo.style.width='60px';
+logo.style.height='60px';
+logo.style.backgroundImage='url(https://s8.hostingkartinok.com/uploads/images/2020/02/8cf1817b5bdad421935faa508ed53261.png)';
+logo.style.backgroundSize='cover';
+
+txt.style.color = '#fff';
+txt.style.textAlign = 'justify';
+txt.style.width = '50%';
+
+cross.style.background = '#d6d6d3';
+cross.style.padding = '2px 6px';
+cross.style.borderRadius = '100%';
+cross.style.border = '1px solid #000';
+cross.style.position = 'absolute';
+cross.style.top = '-42%';
+cross.style.left = '90%';
+cross.style.cursor = 'pointer';
 
 
 if(window.matchMedia('(max-width: 980px)').matches){
-    let txt = document.querySelector('.wapp_txt');
-    let logo = document.querySelector('.wapp_logo');
     wapp2.style.width = '80%';
     wapp2.style.height = '10%';
     logo.width = '200px';
     logo.height = '200px';
     txt.style.fontSize = '2.5em';
-    cross.style.top = '-30%';
-    cross.style.fontSize = '4em';
+    cross.style.top = '-35%';
+    cross.style.fontSize = '3em';
 }
 
 
