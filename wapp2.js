84783a8e3afaf4f13252b93745dbde684c8a4221
Maxorik <gms441@mail.com>
2020-02-19 23:24:53 +0700

    wapp

--- wapp2.js
+++ wapp2.js
@@ -21,7 +21,38 @@ wapp2.addEventListener('click', function call_wapp(event){
 
 let cross = document.querySelector('.cross');
 cross.addEventListener('click', function hide_wapp(){
-    wapp2.style.display = 'none';
+    wapp2.innerHTML = '<a href="'+path2+'" class="pulse-button" target="_blank"></a>';
+
+    let pulsebutton = document.querySelector('.pulse-button');
+    pulsebutton.style.position='fixed';
+    pulsebutton.style.bottom='20px';
+    pulsebutton.style.marginLeft='-20px';
+    pulsebutton.style.marginTop='-20px';
+    pulsebutton.style.display= 'block';
+    pulsebutton.style.width='3em';
+    pulsebutton.style.height= '3em';
+    pulsebutton.style.fontSize='1.3em';
+    pulsebutton.style.color='#fff';
+    pulsebutton.style.textShadow='0 1px 0 #1f4c76';
+    pulsebutton.style.border='1px solid #fff';
+    pulsebutton.style.boxShadow='0 0 0 0 rgba(90, 153, 220, 0.7), inset 0 1px 0 #abcbe9';
+    pulsebutton.style.borderRadius='100%';
+    pulsebutton.style.background='#fff';
+    pulsebutton.style.cursor='pointer';
+    pulsebutton.style.backgroundRepeat= 'no-repeat';
+    pulsebutton.style.backgroundSize='3em 3em';
+    pulsebutton.style.backgroundImage = 'url(https://s8.hostingkartinok.com/uploads/images/2020/02/8cf1817b5bdad421935faa508ed53261.png)';
+    pulsebutton.style.webkitAnimation = 'pulse 1s infinite cubic-bezier(0.6, 0, 0, 1)';
+    wapp2.style.backgroundColor = 'transparent';
+    wapp2.style.width = '22%';  //ширина зеленого окна
+
+    let cssAnimation = document.createElement('style');
+    cssAnimation.type = 'text/css';
+    let rules = document.createTextNode('@-webkit-keyframes pulse {'+
+    'to{box-shadow:0 0 0 15px rgba(90, 153, 220, 0), inset 0 1px 0 #abcbe9}'+
+    '}');
+    cssAnimation.appendChild(rules);
+    document.getElementsByTagName("head")[0].appendChild(cssAnimation);
 })
 
 //стили десктопа
